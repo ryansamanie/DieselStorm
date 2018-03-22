f4621dd6815511d47ade02fad2e5cd4a768cd4d1
ZyronMelancon <altausolaris@gmail.com>
2018-03-22 17:48:21 -0500

    added mockup commander + movement scripts

---
+++ Assets/Scripts/Commander/CommanderMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CommanderMovement : MovementBehaviour {
+
+    Rigidbody m_body;
+    
+
+    public float m_turnForce;
+    public float m_liftForce;
+    public float m_moveForce;
+
+    public float m_maxHeight = 160;
+    public float m_minHeight = 35;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Keep this thing upright no matter what
+        transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+    }
+
+    private void FixedUpdate()
+    {
+        Limit();
+    }
+
+    public override void Move(float m_hinput, float m_vinput, float m_jinput)
+    {
+
+        Vector3 movevec = new Vector3(m_hinput * m_moveForce * m_body.angularVelocity.magnitude * 2, 
+            m_jinput * m_liftForce, m_vinput * m_moveForce);
+        Vector3 turnvec = new Vector3(0, m_hinput * m_body.velocity.magnitude * m_turnForce, 0);
+        m_body.AddRelativeForce(movevec);
+        m_body.AddRelativeTorque(turnvec);
+    }
+
+    void Limit()
+    {
+        //Checks if the ship is below or above limits
+        if (m_body.position.y < m_minHeight)
+            m_body.AddForce(0, 5, 0);
+        else if (m_body.position.y > m_maxHeight)
+            m_body.AddForce(0, -5, 0);
+    }
+}
---
+++ Assets/Scripts/Commander/NetworkCommander.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(CommanderMovement))]
+public class NetworkCommander : NetworkBehaviour {
+
+    Rigidbody m_body;
+
+    public int updatesPerSecond = 60;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_body = GetComponent<Rigidbody>();
+        StartCoroutine(InputSync());
+    }
+
+
+    //Send out input information to other players
+    [Command]
+    void CmdPlayer(float h, float v, float j, Vector3 m, Vector3 p, Vector3 b)
+    {
+        RpcPlayer(h, v, j, m, p, b);
+    }
+
+    //Update input variables from other players
+    [ClientRpc]
+    void RpcPlayer(float h, float v, float j, Vector3 m, Vector3 p, Vector3 b)
+    {
+        if (!isLocalPlayer)
+        {
+            //input.Hinput = h;
+            //input.Vinput = v;
+            //input.Jinput = j;
+            //            m_movement.Steerinput = m;
+            m_body.position = p;
+            m_body.velocity = b;
+        }
+    }
+
+    //X times per second, send input information from the local player to the rest of the players
+    IEnumerator InputSync()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1 / updatesPerSecond);
+            if (isLocalPlayer)
+            {
+                //CmdPlayer(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),
+                //  Input.GetAxis("Jump"), input.m_steerGuide.forward, m_body.position, m_body.velocity);
+            }
+        }
+    }
+
+}
